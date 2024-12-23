52c9d4f99f64e3d98f62c342d040cc3e254acf99
Ashutosh Bisht <ashu9872180@gmail.com>
2024-12-23 14:20:43 +0000

    Fixed Error : - i was using arrow function instead of normal function that's why it was giving error , so fix it! issue resolved!!

--- Pollyfills/filterPollyfills/filterPollyfills.js
+++ Pollyfills/filterPollyfills/filterPollyfills.js
@@ -1,6 +1,4 @@
-const nums = [1, 2, 4, 543, 4234, 324, 2332, 454, 4, 454, 23, 232];
-
-Array.prototype.myFilter((cb) => {
+Array.prototype.myFilter = function (cb) {
   const ans = [];
 
   for (let i = 0; i < this.length; i++) {
@@ -9,8 +7,12 @@ Array.prototype.myFilter((cb) => {
     }
   }
   return ans;
-});
+};
+
+const nums = [1, 2, 4, 543, 4234, 324, 2332, 454, 4, 454, 23, 232];
 
 let ans = nums.myFilter((el, index, arr) => {
   return el > 500;
 });
+
+console.log(ans);
