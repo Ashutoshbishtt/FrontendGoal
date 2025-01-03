97182be70970f441fac56bd4a5e1c8b595de6cc8
Ashutosh Bisht <ashu9872180@gmail.com>
2025-01-03 14:28:22 +0000

    Once pollyfill

--- Pollyfills/makeItRunOnceFunction/app.js
+++ Pollyfills/makeItRunOnceFunction/app.js
@@ -1,17 +1,59 @@
-function likeTheVideo() {
-  let called = 0;
+// function likeTheVideo() {
+//   let called = 0;
 
-  return function () {
-    if (called == 0) {
-      console.log("You liked the video!");
-      called++;
-    } else {
-      console.log("You already liked the video!");
+//   return function () {
+//     if (called == 0) {
+//       console.log("You liked the video!");
+//       called++;
+//     } else {
+//       console.log("You already liked the video!");
+//     }
+//   };
+// }
+
+// const like = likeTheVideo();
+
+// like(); //You liked the video
+// like(); //You already liked the video!
+
+// function once(fn) {
+//   let called = false; // Tracks whether the function has been called
+
+//   return function (...args) {
+//     if (!called) {
+//       called = true;
+//       return fn(...args); // Call the original function only once
+//     }
+//     // Do nothing for subsequent calls
+//   };
+// }
+
+// // Example Usage
+// function likeTheVideo() {
+//   console.log("You liked the video!");
+// }
+
+// const like = once(likeTheVideo);
+
+// like(); // Logs: "You liked the video!"
+// like(); // Does nothing
+
+function once(fn) {
+  let called = false;
+
+  return function (...args) {
+    if (called == false) {
+      called = true;
+      return fn(args);
     }
   };
 }
 
-const like = likeTheVideo();
+function subscribeMe() {
+  console.log("Hey you subscribed to Ashutosh Bisht");
+}
 
-like(); //You liked the video
-like(); //You already liked the video!
+const subscribe = once(subscribeMe);
+
+subscribe();
+subscribe();
